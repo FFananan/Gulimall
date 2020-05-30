324945406e45450581390e9cbe8b6aedfb79b3d9
FFanan <997792054@qq.com>
2020-05-30 16:42:37 +0800

    项目初始结构

---
+++ gulimall-warehousing/src/main/java/cn/fan/gulimall/GulimallWarehousingApplication.java
@@ -0,0 +1,13 @@
+package cn.fan.gulimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWarehousingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWarehousingApplication.class, args);
+    }
+
+}
